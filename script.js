88d28654d80349350f7a82670b232d85a19e35c7
Oliver <oliver.redeyoff@gmail.com>
2020-02-21 23:47:02 +0000

    Correctly sized canvas and centered

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
 function drawLine(){
-  ctx.moveTo(0,0);
-  ctx.lineTo(200,100);
+  ctx.moveTo(10.5,0);
+  ctx.lineTo(20.5,10);
   ctx.stroke();
 }
