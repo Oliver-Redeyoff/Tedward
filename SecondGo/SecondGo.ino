f572e9bbd421849c15d4d7171742eb492cd255ed
Will Cruse <willcruse123@protonmail.com>
2020-02-23 09:55:38 +0000

    Stuff

--- SecondGo/SecondGo.ino
+++ SecondGo/SecondGo.ino
@@ -1,15 +1,16 @@
 #include <Arduino.h>
+#include <stdio.h>
 
 //Motor Pins
 int left[] = {5, 6};
 int right[] = {9, 10};
 
 //Output modulation
-int out[] = {0, 127};
+int out[] = {0, 255};
 int count = 0;
 
 //High Speed, Low Speed
-const int HIGH_VALUE = 127;
+const int HIGH_VALUE = 255;
 const int LOW_VALUE = 0;
 
 //IR sensors
@@ -17,11 +18,17 @@ int leftIR = 12;
 int rightIR = 11;
 
 //Pause time when correcting on left/right IR
+//Pause time when correcting on left/right IR
 const long PAUSE_TIME = 50;
 
-int turns[] = {0};
+// int turns[] = {0, 0};
+// int pointer = 0;
+// int length = 2;
+int turns[1000];
 int pointer = 0;
-int length = 1;
+int length = 0;
+char serialString[1000];
+
 
 void setup() {
   Serial.begin(9600);
@@ -37,31 +44,44 @@ void setup() {
 }
 
 void loop() {
+  Serial.readBytes(serialString, 1000); // read the left or right integer value and put in turns
+  // split serial string as each item is direction
+  Serial.println(serialString);
+  int t;
+  sscanf(serialString, "%d", &t);
+  turns[length] = t;
+  delay(1000);
+  length++;
+  // analogWrite(right[0], HIGH_VALUE);
+  // analogWrite(left[0], HIGH_VALUE);
+  // Serial.println(digitalRead(leftIR);
+// Serial.println(digitalRead(rightIR)==LOW);
   if (digitalRead(leftIR) == HIGH && digitalRead(rightIR) == HIGH) {
-    if (pointer >= length) {
+    if (pointer == length) {
       analogWrite(left[0], LOW_VALUE);
       analogWrite(right[0], LOW_VALUE);
     } else if (turns[pointer] == 0) {
-      analogWrite(left[0], LOW_VALUE);
+      digitalWrite(left[0], LOW_VALUE);
       delay(PAUSE_TIME);
       analogWrite(left[0], HIGH_VALUE);
-      pointer++;
     } else {
-      analogWrite(right[0], LOW_VALUE);
+      digitalWrite(right[0], LOW_VALUE);
       delay(PAUSE_TIME);
       analogWrite(right[0], HIGH_VALUE);
-      pointer++;
+    }
+    pointer++;
+    if (pointer > length) {
+      pointer = 0;
     }
   }
   if (digitalRead(leftIR) == LOW) {
-        analogWrite(right[0], LOW_VALUE);
+        digitalWrite(right[0], LOW_VALUE);
         delay(PAUSE_TIME);
-        digitalWrite(right[0], HIGH_VALUE);
+        analogWrite(right[0], HIGH_VALUE);
     }
     if (digitalRead(rightIR) == LOW) {
         digitalWrite(left[0], LOW_VALUE);
         delay(PAUSE_TIME);
-        digitalWrite(left[0], HIGH_VALUE);
+        analogWrite(left[0], HIGH_VALUE);
     }
 }
-
--- webserver/webserver/webserver.ino
+++ webserver/webserver/webserver.ino
@@ -2,6 +2,10 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <ESP8266WebServer.h>
+// #include <ESP_SSD1306.h>    // Modification of Adafruit_SSD1306 for ESP8266 compatibility
+// #include <Adafruit_GFX.h>   // Needs a little change in original Adafruit library (See README.txt file)
+// #include <SPI.h>            // For SPI comm (needed for not getting compile error)
+// #include <Wire.h>
 
 static const uint8_t Di0   = 16;
 static const uint8_t Di1   = 5;
@@ -17,37 +21,19 @@ static const uint8_t TXPi  = 1;
 
 const char* SSID="JunctionX Exeter";
 const char* PWD="junctionx";
+// ESP_SSD1306 display(-1); // FOR I2C
+
 ESP8266WebServer server(80);
 void handleRecDir();
 
-bool ready = true;
 int inst[1000];
 int len = 1;
 
-//LEFT IR RIGHT IR
-int irSensors[] = {Di6, Di7};
-
-// echo distance sensor pins
-int trig = Di1;
-int echo = Di8;
-
-// right motor pins
-int p1 = Di2;
-int p2 = Di3;
-
-// left motor pins
-int p3 = Di5;
-int p4 = Di4;
-
 void setup() {
   Serial.begin(9600);
-  WiFi.begin(SSID);
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-
-  pinMode(irSensors[0], INPUT);
-  pinMode(irSensors[1], INPUT);
-
+  WiFi.begin(SSID, PWD);
+  // display.begin(SSD1306_SWITCHCAPVCC);  // Switch OLED
+  // display.clearDisplay();
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -59,57 +45,20 @@ void setup() {
   server.on("/recdir", handleRecDir);
   server.begin();
   Serial.println(WiFi.localIP());
-
-
+  // display.println(WiFi.localIP());
+  // display.display();
 }
-
+//
 void loop() {
-  // first check if there is an obstacle in front
-  long duration, distance;
- 
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
- 
-  duration = pulseIn(echo, HIGH);
-  distance = (duration/2)/29.1;
- 
-  if (distance < 20) {
-    Serial.println("Trig");
-    //STOP!!
-    //REVERSE
-  }
-
-  // then check if both IR sensors are either side of the line
-  Serial.println("Left: " + String(digitalRead(irSensors[0])));
-  Serial.println("Right: " + String(digitalRead(irSensors[1])));
-
-  // finally deal with internet releated stuff 
-  if (ready) {
-    server.handleClient();
-  } else {
-    int pointer = 0;
-    while (pointer < len) {
-      int curr = inst[pointer];
-      if (digitalRead(irSensors[curr]) == HIGH) {
-        //TODO TURN TO THERE
-      } else {
-        //TODO MOVE FORWARD
-      }
-      //TODO ENSURE CORRECT FOLLOW
-    }
-    ready = true;
-  }
+  server.handleClient();
 }
+
 
 /*
   Post Request
   Rec json {array: [0, 1]} 0 = Left, 1 = Right
 */
 void handleRecDir() {
-  ready = false;
   StaticJsonDocument<200> doc;
   if (server.arg("plain") == false) {
     Serial.println("null body");
@@ -133,11 +82,14 @@ void handleRecDir() {
      int i = 0;
      for (JsonVariant v : arr) {
        Serial.println(v.as<int>());
+       Serial.write(v.as<int>());
        inst[i] = v.as<int>();
        i++;
        message += v.as<String>();
        message += "\n";
      }
+     // send string version of inst
+
    } else {
      Serial.println(doc.containsKey("array"));
      message += "no body";
@@ -146,22 +98,3 @@ void handleRecDir() {
    server.send(200, "text/plain", message);
    Serial.println(message);
 }
-
-// #include <Arduino.h>
-//
-// const int pins [3] = {14, 2, 0}; //Pin D5, D4, D3
-//
-// void setup() {
-//   for (int i = 0; i < 3; i++) {
-//     pinMode(pins[i], INPUT);
-//   }
-//     Serial.begin(9600);
-// }
-//
-// void loop() {
-//   Serial.println("Left: " + String(digitalRead(pins[0])));
-//   Serial.println("Front: " + String(digitalRead(pins[1])));
-//   Serial.println("Right: " + String(digitalRead(pins[2])));
-//   delay(100);
-// }
-
