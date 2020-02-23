2b190881683978de13875cac97427971c214cdca
Will Cruse <willcruse123@protonmail.com>
2020-02-23 04:08:22 +0000

    Ay

--- SecondGo/SecondGo.ino
+++ SecondGo/SecondGo.ino
@@ -16,7 +16,12 @@ const int LOW_VALUE = 0;
 int leftIR = 12;
 int rightIR = 11;
 
+//Pause time when correcting on left/right IR
 const long PAUSE_TIME = 50;
+
+int turns[] = {0};
+int pointer = 0;
+int length = 1;
 
 void setup() {
   Serial.begin(9600);
@@ -26,11 +31,28 @@ void setup() {
   }
   pinMode(leftIR, INPUT);
   pinMode(rightIR, INPUT);
+
   analogWrite(right[0], HIGH_VALUE);
   analogWrite(left[0], HIGH_VALUE);
 }
 
 void loop() {
+  if (digitalRead(leftIR) == HIGH && digitalRead(rightIR) == HIGH) {
+    if (pointer >= length) {
+      analogWrite(left[0], LOW_VALUE);
+      analogWrite(right[0], LOW_VALUE);
+    } else if (turns[pointer] == 0) {
+      analogWrite(left[0], LOW_VALUE);
+      delay(PAUSE_TIME);
+      analogWrite(left[0], HIGH_VALUE);
+      pointer++;
+    } else {
+      analogWrite(right[0], LOW_VALUE);
+      delay(PAUSE_TIME);
+      analogWrite(right[0], HIGH_VALUE);
+      pointer++;
+    }
+  }
   if (digitalRead(leftIR) == LOW) {
         analogWrite(right[0], LOW_VALUE);
         delay(PAUSE_TIME);
